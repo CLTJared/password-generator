84df11d34727a64e4b82f4a48339a0d6648fc08f
Jared Elliott <bootcamp@jaredclt.com>
2023-12-13 18:22:37 -0500

    Starting confirm boxes for Lowercase, Uppercase, Number, and Special character choices.

--- script.js
+++ script.js
@@ -26,10 +26,34 @@ function promptLength() {
   let pwdLength = '';
   let checkInput =  '';
   do {
-    pwdLength = prompt("How long of a password? Minimum of 8, Maximum of 128: ", "8")
+    pwdLength = prompt("How many characters for your password?\n\nMinimum of 8, Maximum of 128. ", "8")
     checkInput = pwdLength >= 8 && pwdLength <= 128;
   } while (!checkInput || pwdLength === null)
   
   if(pwdLength === null) { return 0; } // Return 0 if user pressed cancel
   return pwdLength; // Return value(s) of Criteria
 }
+
+function promptLower() { // Confirm if wanting lowercase letters
+  pwdLowerCase = confirm("Do you want to include Lower Case letters?\n\nOK = Yes | Cancel = No");
+  return pwdLowerCase;
+}
+function promptUpper() { // Confirm if wanting uppercase letters
+  pwdUpper = confirm("Do you want to include Upper Case letters?\n\nOK = Yes | Cancel = No");
+  return pwdUpper;
+}
+function promptNumber() { // Confirm if wanting numbers letters
+  pwdNumber = confirm("Do you want to include Numbers?\n\nOK = Yes | Cancel = No");
+  return pwdNumber;
+}
+function promptSpecial() { // Confirm if wanting special chars letters
+  pwdSpecial = confirm("Do you want to include Special Characters?\n\nOK = Yes | Cancel = No");
+  return pwdSpecial;
+}
+
+console.log(promptLength());
+
+console.log(promptLower());
+console.log(promptUpper());
+console.log(promptNumber());
+console.log(promptSpecial());
