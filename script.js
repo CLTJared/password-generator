e8e8b8dd7a93e6790413c14be50827d2d0c7c2d6
Jared Elliott <bootcamp@jaredclt.com>
2023-12-14 21:41:35 -0500

    Fixed issue with do while loop that kept from cancelling. Added in function cancel if user selects no criteria for password.

--- script.js
+++ script.js
@@ -19,20 +19,24 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// My Code Goes Below Here
-
 //
 // Get user's requirement for length of password
 //
 function promptLength() { 
   let pwdLength = '';
-  let checkInput =  '';
+  let checkCancel = ''; // for checking the do while loop
+  let checkInput = '';
+
+  // Do While loop so that we can continue to prompt user for length if they enter an invalid number.
   do {
     pwdLength = prompt("How many characters for your password?\n\nMinimum of 8, Maximum of 128. ", "8")
-    checkInput = pwdLength >= 8 && pwdLength <= 128;
-  } while (!checkInput || pwdLength === null)
+    checkInput = pwdLength===null || pwdLength >= 8 && pwdLength <= 128;
+
+    console.log("Check Length: " + pwdLength);
+    console.log("Check Input: " + checkInput); //check if we should exit the prompt loop
+  } while (!checkInput);
   
-  if(pwdLength === null) { return 0; } // Return 0 if user pressed cancel
+  if(pwdLength == null) { return 0; } // Return 0 if user pressed cancel
   return pwdLength; // Return value(s) of Criteria
 }
 
@@ -56,30 +60,32 @@ function promptSpecial() { // Confirm if wanting special chars letters
   return pwdSpecial;
 }
 
-//
-// Generate Password
-//
-  let numbers = '0123456789';
-  let lowerLetter = 'abcdefghijklmnopqrstuvwxyz';
-  let upperLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let specialChars ='!@#$%^&*()-+_=';
+// These strings are used 
+let numbers = '0123456789';
+let lowerLetter = 'abcdefghijklmnopqrstuvwxyz';
+let upperLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let specialChars ='!@#$%^&*()-+_=';
 
 function generatePassword() {
   let userLength = promptLength();
   //
   // Come back and create loop through these prompts if the user says NO to all
   //
+  if (userLength===0) { return "Generate function was cancelled."; } // if user cancelled the function, don't continue on.
+
   let userLower = promptLower()
   let userUpper = promptUpper()
   let userNumber = promptNumber()
   let userSpecial = promptSpecial()
 
+  if (!userLower && !userUpper && !userNumber && !userSpecial) { return "User entered no valid criteria for password."; };
+
   // Console Log for debugging
-  console.log(userLength);
-  console.log(userLower);
-  console.log(userUpper);
-  console.log(userNumber);
-  console.log(userSpecial);
+  console.log("User Length: " + userLength);
+  console.log("Use Lower: " + userLower);
+  console.log("Use Upper: " + userUpper);
+  console.log("Use Number: " + userNumber);
+  console.log("Use Special: " + userSpecial);
 
   let tempPWDString = ''; // Create temp string to hold valid char(s) from user choices
   if(userLower) tempPWDString = tempPWDString + lowerLetter;
@@ -89,10 +95,13 @@ function generatePassword() {
 
   console.log(tempPWDString);
 
+  //
+  // Generate Password
+  //
   let tempPWD = '';
-  for(let i=0;i<userLength;i++) {
+  for(let i=1;i<=userLength;i++) {
     tempPWD = tempPWD + tempPWDString.charAt(Math.floor(Math.random()*tempPWDString.length));
-    console.log(tempPWD); // Log out each step of the password being generated
+    console.log("User Length: " + userLength + " - Current Length: " + i + " - Current Pwd: " + tempPWD); // Log out each step of the password being generated
   }
 
   return tempPWD;
